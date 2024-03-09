f7feac13a46fb0b2eaefd83b981af23c01a218b6
Masakhane Matanzima <sihle.matanzima@gmail.com>
2024-03-09 20:08:36 +0000

    initial setup

---
+++ test.js
@@ -0,0 +1,10 @@
+// test/mytest.test.js
+const assert = global.assert || require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+        assert.equal('bar', 'bar');
+    });
+  });
+});
